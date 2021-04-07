78f328ad0623a7e5aa63e5d189574c53276f693f
gauravdwivedi <gauravdwivedi@hotmail.com>
2021-04-07 21:39:55 +0530

    created User class

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ name: 'YourName' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
